5250b37bbf6d85a5fe60ef7052a1f7768cefa241
OlgaModified <olga@modified.dk>
2021-11-03 09:40:06 +0100

    displaying arrays with v-for

--- app.js
+++ app.js
@@ -1,40 +1,20 @@
 const app = Vue.createApp({
         data(){
             return {
-                showBooks: true,
+                showActions: true,
                 actions: [
                     {title: 'Catching the fly', hero:'Spider Man'},
                     {title: 'Hanging out', hero:'Bat Man'},
                     {title: 'Drying the mask', hero:'Iron Man'}
-                ],
-                title: "Catching the fly",
-                author: "Spider Man",
-                created_at: new Date().toLocaleString(),
-                counter: 12,
-                x:0,
-                y:0
+                ]
             }
         },
         methods: {
-            changeTitle(title) {
-                //console.log('you  clicked me')
-                //this.title = "Changed title version"
-                this.title = title
+            
+            toggleShowActions(){
+                this.showActions = !this.showActions
             },
-            toggleShowBooks(){
-                this.showBooks = !this.showBooks
-            },
-            handleEvent(e, data){
-                console.log(e, e.type)
-                if(data){
-                    console.log(data)
-                }
-            },
-            handleMouseMove(e){
-                this.x = e.offsetX
-                this.y = e.offsetY
-
-            }
+            
         }
 })
 
