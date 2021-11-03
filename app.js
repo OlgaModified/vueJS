f72f598d1a07c08a6f1e5a1fdba0420f8f778f0f
OlgaModified <olga@modified.dk>
2021-11-03 09:28:46 +0100

    added divs for learning mouse events

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-// var app = new Vue({
-//     el: '#app',
-//     data: {
-//       message: 'Hello Vue!'
-//     }
-//   })
-
-//   var app2 = new Vue({
-//     el: '#app-2',
-//     data: {
-//       message: 'You loaded this page on ' + new Date().toLocaleString()
-//     }
-//   })
-
-  const app = Vue.createApp({
+const app = Vue.createApp({
         data(){
             return {
+                showBooks: true,
+                actions: [
+                    {title: 'Catching the fly', hero:'Spider Man'},
+                    {title: 'Hanging out', hero:'Bat Man'},
+                    {title: 'Drying the mask', hero:'Iron Man'}
+                ],
                 title: "Catching the fly",
                 author: "Spider Man",
                 created_at: new Date().toLocaleString(),
-                counter: 12
+                counter: 12,
+                x:0,
+                y:0
             }
         },
         methods: {
@@ -26,8 +20,22 @@
                 //console.log('you  clicked me')
                 //this.title = "Changed title version"
                 this.title = title
+            },
+            toggleShowBooks(){
+                this.showBooks = !this.showBooks
+            },
+            handleEvent(e, data){
+                console.log(e, e.type)
+                if(data){
+                    console.log(data)
+                }
+            },
+            handleMouseMove(e){
+                this.x = e.offsetX
+                this.y = e.offsetY
+
             }
         }
-  })
+})
 
-  app.mount('#app')
+app.mount('#app')
