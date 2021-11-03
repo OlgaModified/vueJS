cc619bc43fcb054a9dc7f5db497b1b417e289e22
OlgaModified <olga@modified.dk>
2021-11-03 10:30:06 +0100

    displaying images

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
         data(){
             return {
+                url: 'http://www.thenetninja.co.uk',
                 showActions: true,
                 actions: [
-                    {title: 'Catching the fly', hero:'Spider Man'},
-                    {title: 'Hanging out', hero:'Bat Man'},
-                    {title: 'Drying the mask', hero:'Iron Man'}
+                    {title: 'Catching the fly', hero:'Spider Man', img:'assets/heroes/spiderman.jpg'},
+                    {title: 'Hanging out', hero:'Bat Man', img:'assets/heroes/batman.jpg'},
+                    {title: 'Drying the mask', hero:'Iron Man', img:'assets/heroes/ironman.jpg'}
                 ]
             }
         },
