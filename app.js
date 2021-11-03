0a2c5622ad2f47efba071f0cfc39c40c0397c144
OlgaModified <olga@modified.dk>
2021-11-03 11:23:48 +0100

    css for elements + dynamic class assign

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = Vue.createApp({
             return {
                 url: 'http://www.thenetninja.co.uk',
                 showActions: true,
+                
                 actions: [
-                    {title: 'Catching the fly', hero:'Spider Man', img:'assets/heroes/spiderman.jpg'},
-                    {title: 'Hanging out', hero:'Bat Man', img:'assets/heroes/batman.jpg'},
-                    {title: 'Drying the mask', hero:'Iron Man', img:'assets/heroes/ironman.jpg'}
+                    {title: 'Catching the fly', hero:'Spider Man', img:'assets/heroes/spiderman.jpg', isDone: true},
+                    {title: 'Hanging out', hero:'Bat Man', img:'assets/heroes/batman.jpg', isDone: false},
+                    {title: 'Drying the mask', hero:'Iron Man', img:'assets/heroes/ironman.jpg', isDone: true}
                 ]
             }
         },
@@ -15,6 +16,9 @@ const app = Vue.createApp({
             toggleShowActions(){
                 this.showActions = !this.showActions
             },
+            markDone(action){
+                action.isDone = !action.isDone
+            }
             
         }
 })
