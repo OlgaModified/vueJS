5eb3f050a3941d28c73090edfca1b71aa3301b83
OlgaModified <olga@modified.dk>
2021-11-04 10:46:38 +0100

    added method to start the game

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <h1>Reaction Timer</h1>
-  
+  <button class="btn" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {
-   
+  components: {  
+    Block
+  },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      //console.log(this.delay)
+    }
   }
 }
 </script>
@@ -22,5 +36,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 60px 0;
+  background-color: #c4e9c5;
 }
+
+.btn {
+  background-color: white;
+  color: black;
+  font-weight: 800;
+  border: 2px solid #4CAF50; /* Green */
+  border-radius: 12px;
+  width: 100px;
+  padding: 15px;
+}
+
+button:disabled,
+button[disabled]{
+  border: 1px solid #999999;
+  background-color: #cccccc;
+  color: #666666;
+}
+
 </style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block" v-if="showBlock">click me</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted () {
+        console.log('mount component')
+        setTimeout(() => {
+
+            this.showBlock = true
+            console.log(this.delay)
+
+        }, this.delay)
+
+    },
+    updated() {
+        console.log('updated component')
+    },
+    unmounted() {
+        console.log('unmounted component')
+    }
+
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background:  #0faf87;;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+  }
+</style>
